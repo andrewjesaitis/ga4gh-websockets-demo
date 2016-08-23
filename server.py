5da823ffe4ce71670dbac5456560836efa7f74d4
Andrew Jesaitis <andrew@andrewjesaitis.com>
2016-08-23 11:08:37 -0600

    Use twisted cooperate to iterate in a non-blocking manner

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerF
 import google.protobuf.json_format as json_format
 from twisted.python import log
 from twisted.internet import reactor
+from twisted.internet.task import cooperate
 
 import datamodel.variant
 import ga4gh.variant_service_pb2 as variant_service_pb2
@@ -15,18 +16,21 @@ class MyServerProtocol(WebSocketServerProtocol):
         self.num_variants = 0
         print("Client connecting: {0}".format(request.peer))
 
+    def sendVariants(self, reference_name, start, end):
+        for rec in datamodel.variant.getPysamVariants(reference_name, '1', start, end):
+            variant = datamodel.variant.convertVariant(rec, None)
+            payload = json_format.MessageToJson(variant).encode('utf8')
+            self.sendMessage(payload, isBinary = False)
+            self.num_variants += 1
+            yield None
+        self.sendClose()  
+      
     def onOpen(self):
         print("WebSocket connection open.")
         reference_name = 'NCBI37'
         start = 0
         end = 10000000
-
-        for rec in datamodel.variant.getPysamVariants(reference_name, '1', start, end):
-                variant = datamodel.variant.convertVariant(rec, None)
-                payload = json_format.MessageToJson(variant).encode('utf8')
-                self.sendMessage(payload, isBinary = False)
-                self.num_variants += 1
-        self.sendClose()
+        cooperate(self.sendVariants(reference_name, start, end))
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
